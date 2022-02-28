6aabb5a961307dd24fb8f482c997ecbb3a1837c8
Viktor <v.e.seleznev@mail.ru>
2022-02-28 09:25:43 +0800

    Создание РестКонтроллера для работы товаром (поиск по id, редактирование по id, создание, удаление (только админ, остальные помечают на удаление)

--- src/main/java/com/amr/project/service/abstracts/ItemReadWriteService.java
+++ src/main/java/com/amr/project/service/abstracts/ItemReadWriteService.java
@@ -1,9 +1,14 @@
 package com.amr.project.service.abstracts;
 
 import com.amr.project.model.entity.Item;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface ItemReadWriteService extends ReadWriteService<Item, Long> {
     List<Item> getMostPopularItems(int count);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void deleteByIdCascadeEnable(Long id);
 }
