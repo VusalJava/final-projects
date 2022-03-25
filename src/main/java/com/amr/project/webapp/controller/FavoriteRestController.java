da308ccb090b482294aee126d9b4b65fcfffcd38
krmvmaria <krmvmaria@mail.ru>
2022-03-25 19:19:44 +0300

    favorites

--- src/main/java/com/amr/project/webapp/controller/FavoriteRestController.java
+++ src/main/java/com/amr/project/webapp/controller/FavoriteRestController.java
@@ -4,7 +4,6 @@ import com.amr.project.converter.FavoriteMapper;
 import com.amr.project.model.dto.FavoriteDto;
 import com.amr.project.model.entity.Favorite;
 import com.amr.project.service.abstracts.FavoriteService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,7 +28,7 @@ public class FavoriteRestController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFromFavorite(@PathVariable Long id) {
-        favoriteService.deleteByIdCascadeEnable(id);
+        favoriteService.deleteByIdCascadeIgnore(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
