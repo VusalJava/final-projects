27c5e76143517bf898f362b49941a587064ffc0f
krmvmaria <krmvmaria@mail.ru>
2022-04-01 17:23:02 +0300

    add security

--- src/main/java/com/amr/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/amr/project/service/impl/UserServiceImpl.java
@@ -4,21 +4,48 @@ import com.amr.project.dao.abstracts.UserReadWriteDao;
 import com.amr.project.model.entity.User;
 import com.amr.project.service.abstracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl extends ReadWriteServiceImpl<User, Long> implements UserService {
+public class UserServiceImpl extends ReadWriteServiceImpl<User, Long> implements UserService, UserDetailsService {
 
     public final UserReadWriteDao userDao;
+    public final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserReadWriteDao dao) {
+    public UserServiceImpl(UserReadWriteDao dao, PasswordEncoder passwordEncoder) {
         super(dao);
         userDao = dao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User getUserByUsername(String name) {
         return userDao.getUserByUsername(name);
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        return userDao.getUserByUsername(name);
+    }
+
+    @Override
+    public User persist(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userDao.persist(user);
+        return user;
+    }
+
+    @Override
+    public void update(User user){
+        if (!user.getPassword().equals(getUserByUsername(user.getUsername()).getPassword())) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+        userDao.update(user);
     }
 }
--- src/main/java/com/amr/project/webapp/config/security/SecurityConfig.java
+++ src/main/java/com/amr/project/webapp/config/security/SecurityConfig.java
@@ -1,7 +1,46 @@
 package com.amr.project.webapp.config.security;
 
+import com.amr.project.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserServiceImpl userServiceImpl;
+    private final SuccessUserHandler successUserHandler;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(UserServiceImpl userServiceImpl,
+                          SuccessUserHandler successUserHandler,
+                          PasswordEncoder passwordEncoder) {
+        this.userServiceImpl = userServiceImpl;
+        this.successUserHandler = successUserHandler;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin().successHandler(successUserHandler)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userServiceImpl).passwordEncoder(passwordEncoder);
+    }
 }
