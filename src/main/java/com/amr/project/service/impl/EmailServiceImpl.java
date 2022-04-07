116c960ce1636fd0ea3cb96ea68ed5b87654cced
VusalJava <alievusal111@gmail.com>
2022-04-07 19:01:35 +0400

    emailsender

---
+++ src/main/java/com/amr/project/service/impl/EmailServiceImpl.java
@@ -0,0 +1,31 @@
+package com.amr.project.service.impl;
+
+import com.amr.project.service.abstracts.EmailService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender emailSender;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    public EmailServiceImpl(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    @Override
+    public void sendEmail(String toAddress, String subject, String text) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(username);
+        mailMessage.setTo(toAddress);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(text);
+
+        emailSender.send(mailMessage);
+    }
+}
