c44f06d0aef5753b298f1f834e7b72f270ee3035
VusalJava <alievusal111@gmail.com>
2022-04-12 17:34:35 +0400

    emailsender

--- src/main/java/com/amr/project/webapp/controller/UserRestController.java
+++ src/main/java/com/amr/project/webapp/controller/UserRestController.java
@@ -4,7 +4,7 @@ import com.amr.project.converter.ImageMapper;
 import com.amr.project.converter.UserMapper;
 import com.amr.project.model.dto.ImageDto;
 import com.amr.project.model.dto.UserDto;
-import com.amr.project.model.entity.User;
+import com.amr.project.service.abstracts.MailService;
 import com.amr.project.service.abstracts.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserRestController {
 
     private UserService userService;
+    private MailService mailService;
     private UserMapper userMapper;
     private ImageMapper imageMapper;
 
@@ -24,6 +25,9 @@ public class UserRestController {
     public ResponseEntity<UserDto> changePassword(@RequestBody UserDto userDto) {
         if(userDto.getPassword() != null || !userDto.getPassword().equals("")) {
             userService.update(userMapper.toEntity(userDto));
+            mailService.sendEmail(userDto.getEmail(),
+                    "password notification",
+                    "your password has been changed");
             return ResponseEntity.ok(userDto);
         }
         return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
@@ -33,23 +37,5 @@ public class UserRestController {
     public ResponseEntity<Void> changeImage(@RequestParam(value = "id") Long id, @RequestBody ImageDto imageDto) {
         userService.findById(id).setImage(imageMapper.toEntity(imageDto));
         return ResponseEntity.ok().build();
-    }
-
-    @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) {
-        User userCreate = userService.persist(userMapper.toEntity(user));
-        return ResponseEntity.ok(userMapper.toDto(userCreate));
-    }
-
-    @PutMapping
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto) {
-        User user = userMapper.toEntity(userDto);
-        return ResponseEntity.ok(userMapper.toDto(user));
-    }
-
-    @DeleteMapping("/{id}/delete")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        userService.deleteByIdCascadeEnable(id);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
