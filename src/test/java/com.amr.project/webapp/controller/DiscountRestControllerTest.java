77f121afc5731d6ce42afd8fee3ed5181e4b36ce
krmvmaria <krmvmaria@mail.ru>
2022-04-12 18:04:15 +0300

    add tests

--- src/test/java/com.amr.project/webapp/controller/DiscountRestControllerTest.java
+++ src/test/java/com.amr.project/webapp/controller/DiscountRestControllerTest.java
@@ -92,5 +92,4 @@ public class DiscountRestControllerTest {
                         .content(objectMapper.writeValueAsString(discounts.get(0))))
                 .andExpect(status().isOk());
     }
-
 }
